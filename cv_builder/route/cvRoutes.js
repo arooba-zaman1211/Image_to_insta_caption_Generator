0b82ab9634438e0ce6db195e35c7c90c495354af
Geeky <124719983+arooba-zaman1211@users.noreply.github.com>
2025-05-11 10:41:00 +0500

    Delete cv_builder directory

--- cv_builder/route/cvRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { generateCV, getTemplates } = require("../controller/cvController");
-
-const router = express.Router();
-
-router.post("/generate", generateCV);
-
-module.exports = router;
